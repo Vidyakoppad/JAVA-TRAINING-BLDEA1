73c98f1f2351e097f443aa9fb7ba8af7e93186b2
vidya <vidya@hp>
2023-03-22 13:32:19 +0530

    Array codes

--- src/Trainingcodes/Test2.java
+++ src/Trainingcodes/Test2.java
@@ -1,10 +1,19 @@
-package Trainingcodes;
 
-public class Test2 {
+package Trainingcodes;   
+
+public class Test2{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i=1;
+		do
+		{
+			System.out.println("Hello good eve");
+			i++;
+		}while(i<=5);
+		
+		}
 
 	}
 
-}
+
+
