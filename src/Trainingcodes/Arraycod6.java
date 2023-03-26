b700e49736a47a9e8cdd69c4b0675912ac23cdfe
vidya <vidya@hp>
2023-03-26 20:05:37 +0530

    Array code

---
+++ src/Trainingcodes/Arraycod6.java
@@ -0,0 +1,19 @@
+package Trainingcodes;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Arraycod6{
+	public static void main(String[] args) {
+		int arr[]= {10,50,67,34,28,78,8,7};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+", ");
+		}
+
+	}
+
+
+
+}
+
