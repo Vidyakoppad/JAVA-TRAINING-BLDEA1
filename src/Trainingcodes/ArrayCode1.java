1a9b0cb25833b36856b89c9ddf2d1b254bf47330
vidya <vidya@hp>
2023-03-26 19:42:14 +0530

    Array code6

---
+++ src/Trainingcodes/ArrayCode1.java
@@ -0,0 +1,42 @@
+package Trainingcodes;
+import java.util.Scanner;
+
+public class ArrayCode1 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the count of classes :");
+		Scanner sc =new Scanner(System.in);
+		int cls = sc.nextInt();
+		System.out.println("Enter the count of students in each class :");
+		int stu = sc.nextInt();
+		int arr[][] = new int [cls][stu];
+		for(int i=0;i<cls;i++) {	
+			System.out.println("inside the class "+(i+1));
+			for(int j=0;j<stu;j++) {
+				System.out.println("Enter the marks of student"+(j+1));
+				arr[i][j] = sc.nextInt();
+
+
+			}
+
+
+		}
+		System.out.println("____________________");
+		for(int i=0;i<cls;i++) {	
+			System.out.println("inside the class "+(i+1));
+			for(int j=0;j<stu;j++) {
+				System.out.println("The marks of student"+(j+1)+" is: "+arr[i][j]);
+
+			}
+
+
+		}
+
+
+
+
+
+
+	}
+
+}
