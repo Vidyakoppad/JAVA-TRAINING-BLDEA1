f57f53d6316b47e3a57d121c7b815139ed40cd5e
vidya <vidya@hp>
2023-03-27 14:13:33 +0530

    prog18 commit

--- src/Trainingcodes/prog18.java
+++ src/Trainingcodes/prog18.java
@@ -1,10 +1,31 @@
 package Trainingcodes;
 
+
+
+import java.util.Scanner;
 public class prog18 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+
+	public static void main(String[] args) 
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println(" enter the value of n:");
+		int n=sc.nextInt();
+	
+		for(int i=1;i<=n;i++) 
+		{
+			for(int j=1;j<=i;j++) 
+			{
+				System.out.print(i+" ");
+				
+			}
+			System.out.println();
+
+		}
 	}
 
 }
+
+
